6ffc2120e0edb094ddd5154e9ebc4b692c621153
Ernest Chuks <ernestchuks2000@gmail.com>
2023-10-25 14:05:12 -0700

    correction 2

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,10 +8,10 @@ void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		putchar(*s);
+		puts_recursion(s + 1);
 	}
 
 	else
-		_putchar('\n');
+		putchar('\n');
 }
